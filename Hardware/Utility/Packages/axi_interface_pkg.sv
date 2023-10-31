888ecb30c99466f11990e9e665129145a88e3741
GabbedT <gabrieletripi02@gmail.com>
2023-10-31 11:32:37 +0100

    AXI configuration package created

---
+++ Hardware/Utility/Packages/axi_interface_pkg.sv
@@ -0,0 +1,22 @@
+`ifndef AXI_INTERFACE_PKG_SV
+    `define AXI_INTERFACE_PKG_SV
+
+package axi_interface_pkg;
+    
+    /* Size specified in bytes */
+    localparam AXI_DATA_SIZE = 4;
+
+    /* Size specified in bits */
+    localparam AXI_ADDR_SIZE = 32;
+
+    /* Burst type */
+    typedef enum logic [1:0] { FIXED, INCR, WRAP } axi_burst_t;
+
+    /* Burst size in bytes */
+    typedef enum logic [1:0] { BYTE, HALFWORD, WORD } axi_burst_size_t;
+
+endpackage : axi_interface_pkg
+
+import axi_interface_pkg::*;
+
+`endif 
