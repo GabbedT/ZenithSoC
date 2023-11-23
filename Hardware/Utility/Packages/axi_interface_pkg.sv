fcacd17f58b7921707d9ce8c692cc96eaacfb3ec
GabbedT <gabrieletripi02@gmail.com>
2023-11-23 12:02:06 +0100

    Package modified

--- Hardware/Utility/Packages/axi_interface_pkg.sv
+++ Hardware/Utility/Packages/axi_interface_pkg.sv
@@ -9,12 +9,6 @@ package axi_interface_pkg;
     /* Size specified in bits */
     localparam AXI_ADDR_SIZE = 32;
 
-    /* Burst type */
-    typedef enum logic [1:0] { FIXED, INCR, WRAP } axi_burst_t;
-
-    /* Burst size in bytes */
-    typedef enum logic [1:0] { BYTE, HALFWORD, WORD } axi_burst_size_t;
-
     /* Response codes */
     typedef enum logic [1:0] { OKAY, EXOKAY, SLVERR, DECERR } axi_response_t;
 
