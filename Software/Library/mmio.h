2c12efe154234f707ff155b5d0859f7134c990e6
GabbedT <gabrieletripi02@gmail.com>
2024-09-19 15:40:37 +0200

    Edited MMIO defines file

--- Software/Library/mmio.h
+++ Software/Library/mmio.h
@@ -1,16 +1,21 @@
-#define MMIO_START 0x00000800
+#define MMIO_START 0x00002000
+
+#define DEVICE_INTERLEAVE (1 << 13)
 
 /* UART base address */
-#define UART_BASE MMIO_START
+#define UART_BASE (MMIO_START)
 
 /* Timer base address */
-#define TIMER_BASE MMIO_START + 128
+#define TIMER_BASE (UART_BASE + DEVICE_INTERLEAVE)
 
 /* GPIO base address */
-#define GPIO_BASE MMIO_START + 256
+#define GPIO_BASE (TIMER_BASE + DEVICE_INTERLEAVE)
 
 /* SPI base address */
-#define SPI_BASE MMIO_START + 384
+#define SPI_BASE (GPIO_BASE + DEVICE_INTERLEAVE)
 
 /* Ethernet base address */
-#define ETHERNET_BASE MMIO_START + 512
+#define ETHERNET_BASE (SPI_BASE + DEVICE_INTERLEAVE)
+
+/* PRNG */
+#define PRNG_BASE (ETHERNET_BASE + DEVICE_INTERLEAVE)
