791757339a67bdf68984a20026ae3166f7bfffc3
Gabriele Tripi <gabrieletripi02@gmail.com>
2024-11-10 11:35:34 +0100

    Modified CPU program

--- Software/Examples/Floating Point/floating_point.cpp
+++ Software/Examples/Floating Point/floating_point.cpp
@@ -1,8 +1,9 @@
 #include "../../Library/SerialOut.h"
+#include "../../Library/mmio.h"
+#include "../../Library/platform.h"
 
 #include "operand_1.h"
 #include "operand_2.h"
-#include "result.h"
 
 #define OPERANDS_NUMBER 200
 
@@ -13,50 +14,72 @@ extern "C" void floating_point() {
 
     riscv_float_operation_t operation = FADD;
 
+    volatile float *nc_buffer = (float *) NC_MEMORY_BASE;
+
     float cpuResult; 
 
-    for (int i = 0; i < OPERANDS_NUMBER; ++i) {
-        switch (operation) {
-            case FADD:
-                cpuResult = operand_1[i] + operand_2[i];
-            break;
 
-            case FSUB:
-                cpuResult = operand_1[i] - operand_2[i];
-            break;
+    union {
+        float    f;
+        uint32_t x;
+    } converterCpu, converterOp1, converterOp2; 
 
-            case FMUL:
-                cpuResult = operand_1[i] * operand_2[i];
-            break;
+    for (operation = FADD; operation <= riscv_float_operation_t::FCLASS; operation = (riscv_float_operation_t) ((int) operation + 1)) {
+        for (int i = 0; i < OPERANDS_NUMBER; ++i) {
 
-            case FMIN:
-                cpuResult = (operand_1[i] > operand_2[i]) ? operand_2[i] : operand_1[i];
-            break;
-
-            case FMAX:
-                cpuResult = (operand_1[i] > operand_2[i]) ? operand_1[i] : operand_2[i];
-            break;
-
-            case FLE:
-                cpuResult = (operand_1[i] <= operand_2[i]) ? 1.0F : 0.0F;
-            break;
-
-            case FLT:
-                cpuResult = (operand_1[i] < operand_2[i]) ? 1.0F : 0.0F;
-            break;
-
-            case FEQ:
-                cpuResult = (operand_1[i] == operand_2[i]) ? 1.0F : 0.0F;
-            break;
-            
-            default:
+            switch (operation) {
+                case FADD:
+                    nc_buffer[i] = operand_1[i] + operand_2[i];
                 break;
-        }
 
-        if (cpuResult == float_result[i]) {
-            SerialOut::printf("Test %d: OK\n", i);
-        } else {
-            SerialOut::printf("Test %d: KO => 0x%x vs 0x%x", i, cpuResult, float_result[i]);
+                case FSUB:
+                    nc_buffer[i] = operand_1[i] - operand_2[i];
+                break;
+
+                case FMUL:
+                    nc_buffer[i] = operand_1[i] * operand_2[i];
+                break;
+
+                case FMIN:
+                    __asm__ volatile ("fmin.s %0, %1, %2" : "=f"(nc_buffer[i]) : "f"(operand_1[i]), "f"(operand_2[i]));
+                break;
+
+                case FMAX:
+                    __asm__ volatile ("fmax.s %0, %1, %2" : "=f"(nc_buffer[i]) : "f"(operand_1[i]), "f"(operand_2[i]));
+                break;
+
+                case FLE:
+                    nc_buffer[i] = (operand_1[i] <= operand_2[i]) ? 1.0F : 0.0F;
+                break;
+
+                case FLT:
+                    nc_buffer[i] = (operand_1[i] < operand_2[i]) ? 1.0F : 0.0F;
+                break;
+
+                case FEQ:
+                    nc_buffer[i] = (operand_1[i] == operand_2[i]) ? 1.0F : 0.0F;
+                break;
+
+                case FCLASS: 
+                    int classif;
+
+                    __asm__ volatile ("fclass.s %0, %1" : "=r" (classif) : "f" (operand_1[i])); 
+
+                    nc_buffer[i] = (float) classif;
+                break;
+                
+                default:
+                break;
+            }
         }
     }
+
+    // for (int i = 0; i < OPERANDS_NUMBER; ++i) {
+    //     converterCpu.f = nc_buffer[i];
+
+    //     SerialOut::writeH(converterCpu.x);
+    //     SerialOut::write('\n');
+    // }
+
+    // SerialOut::write("\n\n\n");
 }
