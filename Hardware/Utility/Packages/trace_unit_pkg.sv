683d30da739e05f1b579e1c42ecbc4d92b4525b6
Gabriele Tripi <gabrieletripi02@gmail.com>
2026-05-02 19:05:16 +0200

    Modified TU package

--- Hardware/Utility/Packages/trace_unit_pkg.sv
+++ Hardware/Utility/Packages/trace_unit_pkg.sv
@@ -3,6 +3,12 @@
 
 package trace_unit_pkg;
     
+    localparam TU_STATUS_ = 0;
+    localparam TU_CONTROL_ = 1;
+    localparam TU_EVENT_ENABLE_ = 2;
+    localparam TU_PC_TRIGGER_ = 3;
+
+
     /* Register map */
     typedef enum logic [1:0] { TU_STATUS, TU_CONTROL, TU_EVENT_ENABLE, TU_PC_TRIGGER } trace_unit_registers_t;
 
@@ -28,8 +34,8 @@ package trace_unit_pkg;
         /* Enable trace trigger */
         logic enable_trigger;
 
-        /* Clear packet buffer */
-        logic clear_buffer;
+        /* Clear timestamp counter */
+        logic clear_timestamp;
 
         /* Enable interrupts */
         logic enable_int_full;
@@ -37,7 +43,7 @@ package trace_unit_pkg;
     } trace_unit_control_t;
 
 
-    /* Control Register */
+    /* Event Control Register */
     typedef struct packed {
         /* Instruction fault events */
         logic instr_misaligned;
@@ -58,6 +64,9 @@ package trace_unit_pkg;
         /* System call events */
         logic u_system_call;
         logic m_system_call;
+        
+        /* Interrupt handler */
+        logic handler_return;
 
         /* Core sleep WFI */
         logic sleep;
@@ -68,17 +77,47 @@ package trace_unit_pkg;
 
         /* CSR */
         logic csr;
-    } trace_unit_event_enable_t;
+    } trace_unit_event_t;
 
 
-    /* Event Packet (2/6 Bytes) */
+    typedef enum logic [1:0] { EVENT_PACKET, DIVERGENCE_PACKET, OVERFLOW_PACKET } trace_unit_packet_type_t;
+
+    /* Event Packet (2/5 Bytes) */
     /* SYNC (1 BYTE) | TYPE EVENT + EVENT_NUMBER (1 BYTE) | TIMESTAMP (IF ENABLED) (3 BYTE) */
 
-    /* Branch Packet (10/14 Bytes) */
-    /* SYNC (1 BYTE) | TYPE BRANCH (1 BYTE) | START PC (4 BYTE) | END PC (4 BYTE) | TIMESTAMP (IF ENABLED) (3 BYTE) */
+    /* Divergence Packet (10/13 Bytes) */
+    /* SYNC (1 BYTE) | TYPE DIVERGENCE (1 BYTE) | START PC (4 BYTE) | END PC (4 BYTE) | TIMESTAMP (IF ENABLED) (3 BYTE) */
 
     /* Time overflow Packet (2 Bytes) */
     /* SYNC (1 BYTE) | TYPE OVERFLOW + OV_FLAG (1 BYTE) | */
+    typedef union packed {
+
+        struct packed {
+            trace_unit_packet_type_t type_;
+            logic [5:0] event_number;
+
+            logic [23:0] timestamp;
+
+            logic [63:0] padding;
+        } event_packet;
+
+        struct packed {
+            trace_unit_packet_type_t type_;
+            logic [5:0] padding;
+
+            logic [31:0] start_pc;
+            logic [31:0] end_pc;
+
+            logic [23:0] timestamp;
+        } divergence_packet;
+
+        struct packed {
+            trace_unit_packet_type_t type_;
+            logic [93:0] padding;
+        } overflow_packet;
+
+        logic [11:0][7:0] raw;
+    } trace_unit_packet_t;
 
 endpackage : trace_unit_pkg
 
