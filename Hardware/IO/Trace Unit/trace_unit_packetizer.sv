c4ebc3d1b20dafd0dcf05615f55a5f9c00c693f4
Gabriele Tripi <gabrieletripi02@gmail.com>
2026-05-09 11:15:31 +0200

    Refactor event generation logic and enhance overflow handling in trace unit packetizer

--- Hardware/IO/Trace Unit/trace_unit_packetizer.sv
+++ Hardware/IO/Trace Unit/trace_unit_packetizer.sv
@@ -68,56 +68,57 @@ module trace_unit_packetizer #(
 
     assign overflow = (timestamp_counter == 24'hFFFFFF) & (enable_timestamp_event_i | enable_timestamp_branch_i);
 
+
 //====================================================================================
 //      EVENT SIGNAL GENERATION
 //====================================================================================
 
     /* Event arrives from the trace interface, we need to decide if 
      * it should generate a trace packet or not based on the enable signals */
-    logic event_generate;
+    logic event_generated;
 
         always_comb begin
-            event_generate = 1'b0;
+            event_generated = 1'b0;
 
             if (trace_interface_i.valid & enable_event_tracing_i) begin
                 case (trace_interface_i.info)
-                    `INSTR_MISALIGNED: event_generate = enable_i.instr_misaligned;
+                    `INSTR_MISALIGNED: event_generated = enable_i.instr_misaligned;
 
-                    `INSTR_ACCESS_FAULT: event_generate = enable_i.instr_access_fault;
+                    `INSTR_ACCESS_FAULT: event_generated = enable_i.instr_access_fault;
 
-                    `INSTR_ILLEGAL: event_generate = enable_i.instr_illegal;
+                    `INSTR_ILLEGAL: event_generated = enable_i.instr_illegal;
 
-                    `BREAKPOINT: event_generate = enable_i.breakpoint;
+                    `BREAKPOINT: event_generated = enable_i.breakpoint;
 
-                    `LOAD_OPERATION: event_generate = enable_i.load_operation;
+                    `LOAD_OPERATION: event_generated = enable_i.load_operation;
 
-                    `LOAD_MISALIGNED: event_generate = enable_i.load_misaligned;
+                    `LOAD_MISALIGNED: event_generated = enable_i.load_misaligned;
 
-                    `LOAD_ACCESS_FAULT: event_generate = enable_i.load_access_fault;
+                    `LOAD_ACCESS_FAULT: event_generated = enable_i.load_access_fault;
 
-                    `STORE_OPERATION: event_generate = enable_i.store_operation;
+                    `STORE_OPERATION: event_generated = enable_i.store_operation;
 
-                    `STORE_MISALIGNED: event_generate = enable_i.store_misaligned;
+                    `STORE_MISALIGNED: event_generated = enable_i.store_misaligned;
 
-                    `STORE_ACCESS_FAULT: event_generate = enable_i.store_access_fault; 
+                    `STORE_ACCESS_FAULT: event_generated = enable_i.store_access_fault; 
 
-                    `U_SYSTEM_CALL: event_generate = enable_i.u_system_call;
+                    `U_SYSTEM_CALL: event_generated = enable_i.u_system_call;
 
-                    `M_SYSTEM_CALL: event_generate = enable_i.m_system_call;
+                    `M_SYSTEM_CALL: event_generated = enable_i.m_system_call;
 
-                    `SLEEP: event_generate = enable_i.sleep;
+                    `SLEEP: event_generated = enable_i.sleep;
 
-                    `HANDLER_RETURN: event_generate = enable_i.handler_return;
+                    `HANDLER_RETURN: event_generated = enable_i.handler_return;
 
-                    `BRANCH_OPERATION: event_generate = enable_i.branch;
+                    `BRANCH_OPERATION: event_generated = enable_i.branch;
 
-                    `JUMP_OPERATION: event_generate = enable_i.jump;
+                    `JUMP_OPERATION: event_generated = enable_i.jump;
 
-                    `CSR_OPERATION: event_generate = enable_i.csr;
+                    `CSR_OPERATION: event_generated = enable_i.csr;
 
-                    `NO_EVENT: event_generate = 1'b0;
+                    `NO_EVENT: event_generated = 1'b0;
 
-                    default: event_generate = 1'b0;
+                    default: event_generated = 1'b0;
                 endcase
             end
         end
@@ -188,25 +189,39 @@ module trace_unit_packetizer #(
 //      PACKET BUILDER
 //====================================================================================
 
+    logic pending_overflow, overflow_serviced; 
+
+        always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin
+            if (!rst_n_i) begin
+                pending_overflow <= 1'b0;
+            end else begin
+                if (overflow & (divergence | event_generated)) begin
+                    /* If there is an overflow and we are generating a divergence or event packet, 
+                     * we dont't need to prioritize the overflow packet, as we'll lose the PC/Event 
+                     * information, which is more useful for debugging than the overflow information */
+                    pending_overflow <= 1'b1;
+                end else if (overflow_serviced) begin
+                    pending_overflow <= 1'b0;
+                end
+            end
+        end
+
+
     logic write_packet; trace_unit_packet_t trace_packet;
 
         always_comb begin
             /* Default values */
             trace_packet = '0;
             write_packet = 1'b0;
+            overflow_serviced = 1'b0;
             
-            /* Priority: Overflow > Divergence > Event */
+            /* Priority: Divergence > Event > Overflow */
 
             /* Don't generate packets if the core is halted, otherwise it can cause
              * deadlocks because the core is waiting for the packet to be consumed 
              * to be able to proceed and generate new packets, but the packet cannot
              * be consumed because the core is halted */
-            if (overflow) begin
-                /* Generate an overflow packet */
-                trace_packet.overflow_packet.type_ = OVERFLOW_PACKET;
-
-                write_packet = !halt_core_o;
-            end else if (divergence & enable_branch_tracing_i) begin
+            if (divergence & enable_branch_tracing_i) begin
                 /* Generate a divergence packet */
                 trace_packet.divergence_packet.type_ = DIVERGENCE_PACKET;
                 trace_packet.divergence_packet.start_pc = last_pc;
@@ -214,13 +229,20 @@ module trace_unit_packetizer #(
                 trace_packet.divergence_packet.timestamp = timestamp_counter;
 
                 write_packet = !halt_core_o;
-            end else if (event_generate) begin
+            end else if (event_generated) begin
                 /* Generate an event packet */
                 trace_packet.event_packet.type_ = EVENT_PACKET;
                 trace_packet.event_packet.event_number = trace_interface_i.info;
                 trace_packet.event_packet.timestamp = timestamp_counter;
 
                 write_packet = !halt_core_o; 
+            end else if (overflow | pending_overflow) begin
+                /* Generate an overflow packet */
+                trace_packet.overflow_packet.type_ = OVERFLOW_PACKET;
+
+                write_packet = !halt_core_o;
+
+                overflow_serviced = !halt_core_o;
             end
         end
 
