1cf231de66cac284881313d44637db6ac4f7dac4
Gabriele Tripi <gabrieletripi02@gmail.com>
2026-06-25 20:53:00 +0200

    Added .f

---
+++ cosim/rtl/cosim.f
@@ -0,0 +1,16 @@
+../../hw/utils/pkg/cache_pkg.sv
+
+-F ../../hw/cpu/_cpu.f
+-F ../../hw/common/_common.f
+
+../../hw/memory/ddr/cache_ddr_interface.sv
+../../hw/memory/memory_bank.sv
+
+../../hw/cpu/cpu_complex.sv
+
+cosim_io.sv
+cosim_rom.sv
+cosim_ddr.sv
+cosim_top.sv
+
+cosim_public.vlt
