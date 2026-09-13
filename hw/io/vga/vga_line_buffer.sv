807e285fd66fec855938977eee2291f591f54b95
Gabriele Tripi <gabrieletripi02@gmail.com>
2026-09-13 12:11:37 +0200

    Added logic to handle 320x240 resolution to avoid repeated fetches of same pixels

--- hw/io/vga/vga_line_buffer.sv
+++ hw/io/vga/vga_line_buffer.sv
@@ -2,7 +2,15 @@
     `define VGA_LINE_BUFFER_SV
 
 module vga_line_buffer #(
-    parameter SIZE = 2**16
+    /* How many pixels */
+    parameter DEPTH = 512,
+
+    /* Width of a pixel */
+    parameter WIDTH = 12,
+
+    /* Refill threshold to account for
+     * DDR latency */
+    parameter THRESHOLD = 16
 ) (
     input logic clk_i,
     input logic rst_n_i,
@@ -11,7 +19,6 @@ module vga_line_buffer #(
 
     /* Write interface */
     input logic write_i,
-    input logic [$clog2(SIZE) - 1:0] address_i,
     input pixel_t pixel_i,
 
     /* Read interface */
@@ -19,7 +26,8 @@ module vga_line_buffer #(
     output pixel_t pixel_o,
 
     /* Status */
-    output logic [$clog2(SIZE) - 1:0] size_o,
+    output logic refill_o,
+    output logic full_o,
     output logic empty_o
 );
 
@@ -28,57 +36,133 @@ module vga_line_buffer #(
 //====================================================================================
 
     /* Memory */
-    logic [$bits(pixel_t) - 1:0] buffer [SIZE - 1:0];
+    logic [WIDTH - 1:0] buffer [DEPTH - 1:0];
 
-    /* Addresses handled by internal logic */
-    logic [$clog2(SIZE) - 1:0] read_address;
+    localparam PTR_SIZE = (DEPTH > 1) ? $clog2(DEPTH) : 1;
+    localparam SIZE_WIDTH = $clog2(DEPTH + 1);
+    localparam LOW_RESOLUTION_SIZE = 320;
+    localparam LOW_RESOLUTION_COUNT_SIZE = $clog2(LOW_RESOLUTION_SIZE);
 
-        
-        /* Write port */
+    /* Buffer pointers */
+    logic [$clog2(DEPTH) - 1:0] write_ptr, read_ptr, line_start_ptr;
+
+    /* Buffer size */
+    logic [$clog2(DEPTH + 1) - 1:0] size;
+
+
+//====================================================================================
+//      MEMORY ACCESS
+//====================================================================================
+
+    logic write_enable, read_enable, pop_enable;
+
+    /* Write when not full and buffer is able to pop */
+    assign write_enable = enable_video_i & write_i & (!full_o | pop_enable);
+
+    /* Read is NOT popping, this is used for low resolution logic */
+    assign read_enable = enable_video_i & read_i & !empty_o;
+
+    /* In 320x240 mode, a line must be read two times since the same pixel will be 
+     * written to positions: (X, Y), (X+1, Y) (X, Y+1) (X+1, Y+1) */
+    assign pop_enable = read_enable & ((resolution_i != _320x240_) | low_resolution_second_pass);
+
+
         always_ff @(posedge clk_i) begin
-            if (write_i) begin
-                buffer[address_i] <= pixel_i;
+            if (write_enable) begin
+                buffer[write_ptr] <= pixel_i;
             end
-        end 
+        end
 
-        /* Read port */
         always_ff @(posedge clk_i) begin
-            if (read_i & !empty_o) begin
-                pixel_o <= buffer[read_address];
+            if (read_enable) begin
+                pixel_o <= buffer[read_ptr];
             end
-        end 
+        end
 
 
-        /* Read address logic */
+//====================================================================================
+//      POINTERS LOGIC
+//====================================================================================
+
+    /* Next pointer */
+    logic [$clog2(DEPTH) - 1:0] inc_write_ptr, inc_read_ptr;
+
+    /* 320x240 logic */
+    logic [$clog2(LOW_RESOLUTION_SIZE) - 1:0] low_resolution_read_count;
+    logic low_resolution_second_pass;
+
+
+    /* Wrap around or increment */
+    assign inc_write_ptr = (write_ptr == (DEPTH - 1)) ? '0 : write_ptr + 1'b1;
+    assign inc_read_ptr = (read_ptr == (DEPTH - 1)) ? '0 : read_ptr + 1'b1;
+
         always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin
-            if (!rst_n_i) begin 
-                read_address <= '0;
-            end else if (!enable_video_i) begin
-                read_address <= '0;
-            end else begin 
-                if (read_i) begin
-                    if (resolution_i == _640x480_) begin
-                        read_address <= (read_address == (MAX_ADDR_1 - 1)) ? '0 : read_address + 1'b1;
+            if (!rst_n_i) begin
+                write_ptr <= '0;
+            end else if (write_enable) begin
+                write_ptr <= inc_write_ptr;
+            end
+        end
+
+        always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin
+            if (!rst_n_i) begin
+                read_ptr <= '0;
+                
+                line_start_ptr <= '0;
+
+                low_resolution_read_count <= '0;
+                low_resolution_second_pass <= 1'b0;
+            end else if (read_enable) begin
+                if (resolution_i == _320x240_) begin
+                    if (low_resolution_read_count == (LOW_RESOLUTION_SIZE - 1)) begin
+                        /* First pass: goes back to the line start 
+                         * Second pass: advance FIFO pointer */
+                        read_ptr <= low_resolution_second_pass ? inc_read_ptr : line_start_ptr;
+                        line_start_ptr <= low_resolution_second_pass ? inc_read_ptr : line_start_ptr;
+                        
+                        low_resolution_read_count <= '0;
+                        low_resolution_second_pass <= !low_resolution_second_pass;
                     end else begin
-                        read_address <= (read_address == (MAX_ADDR_2 - 1)) ? '0 : read_address + 1'b1;
+                        read_ptr <= inc_read_ptr;
+
+                        low_resolution_read_count <= low_resolution_read_count + 1'b1;
                     end
+                end else begin
+                    /* Normal FIFO operation */
+                    read_ptr <= inc_read_ptr;
+                    line_start_ptr <= inc_read_ptr;
+
+                    low_resolution_read_count <= '0;
+                    low_resolution_second_pass <= 1'b0;
                 end
-            end 
-        end 
+            end else if (resolution_i != _320x240_) begin
+                line_start_ptr <= read_ptr;
+
+                low_resolution_read_count <= '0;
+                low_resolution_second_pass <= 1'b0;
+            end
+        end
+
+
+//====================================================================================
+//      FIFO STATUS LOGIC
+//====================================================================================
 
         always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin
-            if (!rst_n_i) begin 
-                size_o <= '0;
-            end else begin 
-                case ({write_i, read_i})
-                    2'b10: size_o <= size_o + 1'b1;
+            if (!rst_n_i) begin
+                size <= '0;
+            end else begin
+                case ({write_enable, pop_enable})
+                    2'b10: size <= size + 1'b1;
+                    2'b01: size <= size - 1'b1;
+                    default: size <= size;
+                endcase
+            end
+        end
 
-                    2'b01: size_o <= size_o - 1'b1;
-                endcase 
-            end 
-        end 
-
-    assign empty_o = size_o == '0;
+    assign full_o = size == DEPTH;
+    assign empty_o = size == '0;
+    assign refill_o = size <= THRESHOLD;
 
 endmodule 
 
