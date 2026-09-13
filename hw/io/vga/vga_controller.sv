42a403b3c403c12131565c8b28e4a0e712f5c5e3
Gabriele Tripi <gabrieletripi02@gmail.com>
2026-09-13 12:10:13 +0200

    Removed 800x600 resolution and added 240x320 resolution in the controller

--- hw/io/vga/vga_controller.sv
+++ hw/io/vga/vga_controller.sv
@@ -5,7 +5,6 @@ module vga_controller (
     input logic clk_i,
     input logic rst_n_i,
 
-    input resolution_t resolution_i,
     input logic display_i,
     input logic next_pixel_i,
     output logic video_on_o,
@@ -35,46 +34,24 @@ module vga_controller (
 //====================================================================================
 
     /* 640x480 display size */
-    localparam H_DISPLAY_SIZE_1 = 640;
-    localparam V_DISPLAY_SIZE_1 = 480;
+    localparam H_DISPLAY_SIZE = 640;
+    localparam V_DISPLAY_SIZE = 480;
 
     /* Horizontal Scan */
-    localparam H_FRONT_PORCH_1 = 16;
-    localparam H_BACK_PORCH_1 = 48;
-    localparam H_RETRACE_1 = 96;
+    localparam H_FRONT_PORCH = 16;
+    localparam H_BACK_PORCH = 48;
+    localparam H_RETRACE = 96;
 
     /* Number of total pixel in an horizontal scan */
-    localparam H_SCAN_PIXEL_1 = H_DISPLAY_SIZE_1 + H_FRONT_PORCH_1 + H_BACK_PORCH_1 + H_RETRACE_1;
+    localparam H_SCAN_PIXEL = H_DISPLAY_SIZE + H_FRONT_PORCH + H_BACK_PORCH + H_RETRACE;
 
     /* Vertical Scan */
-    localparam V_FRONT_PORCH_1 = 10;
-    localparam V_BACK_PORCH_1 = 33;
-    localparam V_RETRACE_1 = 2;
+    localparam V_FRONT_PORCH = 10;
+    localparam V_BACK_PORCH = 33;
+    localparam V_RETRACE = 2;
 
     /* Number of total pixel in a vertical scan */
-    localparam V_SCAN_PIXEL_1 = V_DISPLAY_SIZE_1 + V_FRONT_PORCH_1 + V_BACK_PORCH_1 + V_RETRACE_1;
-
-
-
-    /* 800x600 display size */
-    localparam H_DISPLAY_SIZE_2 = 800;
-    localparam V_DISPLAY_SIZE_2 = 600;
-
-    /* Horizontal Scan */
-    localparam H_FRONT_PORCH_2 = 56;
-    localparam H_BACK_PORCH_2 = 64;
-    localparam H_RETRACE_2 = 120;
-
-    /* Number of total pixel in an horizontal scan */
-    localparam H_SCAN_PIXEL_2 = H_DISPLAY_SIZE_2 + H_FRONT_PORCH_2 + H_BACK_PORCH_2 + H_RETRACE_2;
-
-    /* Vertical Scan */
-    localparam V_FRONT_PORCH_2 = 37;
-    localparam V_BACK_PORCH_2 = 23;
-    localparam V_RETRACE_2 = 6;
-
-    /* Number of total pixel in a vertical scan */
-    localparam V_SCAN_PIXEL_2 = V_DISPLAY_SIZE_2 + V_FRONT_PORCH_2 + V_BACK_PORCH_2 + V_RETRACE_2;
+    localparam V_SCAN_PIXEL = V_DISPLAY_SIZE + V_FRONT_PORCH + V_BACK_PORCH + V_RETRACE;
 
 
 //====================================================================================
@@ -88,60 +65,30 @@ module vga_controller (
 
         always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin
             if (!rst_n_i) begin 
-                hsync <= H_SCAN_PIXEL_1 - 1;
-                vsync <= V_SCAN_PIXEL_1 - 1;
+                hsync <= H_SCAN_PIXEL - 1;
+                vsync <= V_SCAN_PIXEL - 1;
             end else if (!display_i) begin 
-                hsync <= H_SCAN_PIXEL_1 - 1;
-                vsync <= V_SCAN_PIXEL_1 - 1;
+                hsync <= H_SCAN_PIXEL - 1;
+                vsync <= V_SCAN_PIXEL - 1;
             end else if (next_pixel_i) begin 
-                case (resolution_i)
-                    _640x480_: begin
-                        hsync <= (hsync == H_SCAN_PIXEL_1 - 1) ? '0 : (hsync + 1'b1);
+                hsync <= (hsync == H_SCAN_PIXEL - 1) ? '0 : (hsync + 1'b1);
 
-                        if (hsync == H_SCAN_PIXEL_1 - 1) begin
-                            /* VSYNC Counter is increased if horizontal counter is max */
-                            vsync <= (vsync == V_SCAN_PIXEL_1 - 1) ? '0 : (vsync + 1'b1);
-                        end
-                    end
-
-                    _800x600_: begin
-                        hsync <= (hsync == H_SCAN_PIXEL_2 - 1) ? '0 : (hsync + 1'b1);
-
-                        if (hsync == H_SCAN_PIXEL_2 - 1) begin
-                            /* VSYNC Counter is increased if horizontal counter is max */
-                            vsync <= (vsync == V_SCAN_PIXEL_2 - 1) ? '0 : (vsync + 1'b1);
-                        end
-                    end
-                endcase 
+                if (hsync == H_SCAN_PIXEL - 1) begin
+                    /* VSYNC Counter is increased if horizontal counter is max */
+                    vsync <= (vsync == V_SCAN_PIXEL - 1) ? '0 : (vsync + 1'b1);
+                end
             end 
         end 
 
 
-        always_comb begin
-            case (resolution_i)
-                _640x480_: frame_done_o = (vsync == V_SCAN_PIXEL_1 - 1) & (hsync == H_SCAN_PIXEL_1 - 1);
+        /* Every pixel has beed drawn */
+        assign frame_done_o = (vsync == V_SCAN_PIXEL - 1) & (hsync == H_SCAN_PIXEL - 1);
 
-                _800x600_: frame_done_o = (vsync == V_SCAN_PIXEL_2 - 1) & (hsync == H_SCAN_PIXEL_2 - 1);
-            endcase
-        end
+        /* Disable HSYNC during horizontal retrace */
+        assign hsync_o = !((hsync >= H_DISPLAY_SIZE + H_FRONT_PORCH) & (hsync <= (H_DISPLAY_SIZE + H_FRONT_PORCH + H_RETRACE - 1)));
 
-
-        always_comb begin
-            /* Disable HSYNC during horizontal retrace */
-            case (resolution_i)
-                _640x480_: hsync_o = !((hsync >= H_DISPLAY_SIZE_1 + H_FRONT_PORCH_1) & (hsync <= (H_DISPLAY_SIZE_1 + H_FRONT_PORCH_1 + H_RETRACE_1 - 1)));
-
-                _800x600_: hsync_o = !((hsync >= H_DISPLAY_SIZE_2 + H_FRONT_PORCH_2) & (hsync <= (H_DISPLAY_SIZE_2 + H_FRONT_PORCH_2 + H_RETRACE_2 - 1)));
-            endcase 
-
-
-            /* Disable VSYNC during vertical retrace */
-            case (resolution_i)
-                _640x480_: vsync_o = !((vsync >= V_DISPLAY_SIZE_1 + V_FRONT_PORCH_1) & (vsync <= (V_DISPLAY_SIZE_1 + V_FRONT_PORCH_1 + V_RETRACE_1 - 1)));
-
-                _800x600_: vsync_o = !((vsync >= V_DISPLAY_SIZE_2 + V_FRONT_PORCH_2) & (vsync <= (V_DISPLAY_SIZE_2 + V_FRONT_PORCH_2 + V_RETRACE_2 - 1)));
-            endcase 
-        end
+        /* Disable VSYNC during vertical retrace */
+        assign vsync_o = !((vsync >= V_DISPLAY_SIZE + V_FRONT_PORCH) & (vsync <= (V_DISPLAY_SIZE + V_FRONT_PORCH + V_RETRACE - 1)));
 
 
 //====================================================================================
@@ -156,6 +103,7 @@ module vga_controller (
     assign x_match = (hsync >= sprite_x_i) & (hsync <= (sprite_x_i + 8));
     assign y_match = (vsync >= sprite_y_i) & (vsync <= (sprite_y_i + 8));
 
+    /* Matched sprite pixel position will require a read to the sprite table to advance pointer */
     assign read_sprite_o = x_match & y_match & next_pixel_i;
 
     assign sprite_on = !transparent_i & x_match & y_match & enabled_i;
@@ -168,19 +116,13 @@ module vga_controller (
 //      PIXEL LOGIC
 //====================================================================================
 
-        always_comb begin
-            case (resolution_i)
-                _640x480_: video_on_o = (hsync <= (H_DISPLAY_SIZE_1 - 1)) & (vsync <= (V_DISPLAY_SIZE_1 - 1));
-
-                _800x600_: video_on_o = (hsync <= (H_DISPLAY_SIZE_2 - 1)) & (vsync <= (V_DISPLAY_SIZE_2 - 1));
-            endcase 
-        end
-
+    assign video_on_o = (hsync <= (H_DISPLAY_SIZE - 1)) & (vsync <= (V_DISPLAY_SIZE - 1));
 
         always_comb begin
             pixel_o = '0;
 
             if (video_on_o) begin
+                /* Multiplex between sprite or pixel buffer */
                 if (sprite_on) begin
                     pixel_o = sprite_pixel;
                 end else begin
@@ -193,27 +135,13 @@ module vga_controller (
         always_comb begin
             read_pixel_o = 1'b0;
 
-            case (resolution_i)
-                _640x480_: begin
-                    if (video_on_o & (hsync != H_DISPLAY_SIZE_1 - 1)) begin
-                        read_pixel_o = next_pixel_i;
-                    end else begin
-                        if (((hsync == (H_SCAN_PIXEL_1 - 1)) & (vsync < (V_DISPLAY_SIZE_1 - 1))) | frame_done_o) begin
-                            read_pixel_o = next_pixel_i;
-                        end
-                    end
+            if (video_on_o & (hsync != H_DISPLAY_SIZE - 1)) begin
+                read_pixel_o = next_pixel_i;
+            end else begin
+                if (((hsync == (H_SCAN_PIXEL - 1)) & (vsync < (V_DISPLAY_SIZE - 1))) | frame_done_o) begin
+                    read_pixel_o = next_pixel_i;
                 end
-
-                _800x600_: begin
-                    if (video_on_o & (hsync != H_DISPLAY_SIZE_2 - 1)) begin
-                        read_pixel_o = next_pixel_i;
-                    end else begin
-                        if (((hsync == (H_SCAN_PIXEL_2 - 1)) & (vsync < (V_DISPLAY_SIZE_2 - 1))) | frame_done_o) begin
-                            read_pixel_o = next_pixel_i;
-                        end
-                    end
-                end
-            endcase 
+            end
         end
 
 endmodule : vga_controller
